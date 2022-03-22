b802ae75bcac76e0c95881f717d1eaa3a437593d
Juilee6 <81208483+Juilee6@users.noreply.github.com>
2022-03-22 15:11:28 +0530

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -1,4 +1,4 @@
-resource "tfe_organization" "tf-cloud-testing" {
+/*resource "tfe_organization" "tf-cloud-testing" {
   name  = "tf-cloud-testing1"
   email = "jkaloti@deqode.com"
 }
@@ -7,4 +7,21 @@ resource "tfe_workspace" "test" {
   name         = "jk-testing"
   organization = tfe_organization.tf-cloud-testing.name
   tag_names    = ["test", "app"]
+}*/
+terraform {
+  backend "remote" {
+    organization = tfe_organization.tf-cloud-testing
+
+    workspaces {
+      name = "my-workspace-name1"
+    }
+  }
+
+  required_version = "~> 1.0.2"
+}
+
+resource "tfe_organization" "my_tf_cloud_org" {
+  name  = "my-tf-cloud-org"
+  email = "jkaloti@deqode.com"
+ // collaborator_auth_policy = "two_factor_mandatory"
 }
