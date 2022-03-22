d8ea233d6d3d937a2314c5796bb86b016f9cd221
Juilee6 <81208483+Juilee6@users.noreply.github.com>
2022-03-22 17:43:36 +0530

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -1,11 +1,10 @@
-resource "tfe_organization" "test-organization" {
+resource "tfe_organization" "test" {
   name  = "my-org-name"
   email = "jkaloti@deqode.com"
-  tfc_token = zJKqTmpt9L4S8w.atlasv1.WOHd0ygS9Undzqk1OEolayaZhwyz2KvwC2Hu9wGhvjQN8ug7IfEphaCVcJkMKCWKXMQ
 }
 
-resource "tfe_workspace" "test" {
+/*resource "tfe_workspace" "test" {
   name         = "my-workspace-name"
   organization = tfe_organization.test-organization.name
   tag_names    = ["test", "app"]
-}
+}*/
