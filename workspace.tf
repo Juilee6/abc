3f8bc557fcc595e9bfd89f1dbc756b3abc6b079e
Juilee6 <81208483+Juilee6@users.noreply.github.com>
2022-03-22 14:55:52 +0530

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -1,5 +1,10 @@
+resource "tfe_organization" "tf-cloud-testing" {
+  name  = "tf-cloud-testing"
+  
+}
+
 resource "tfe_workspace" "test" {
   name         = "jk-testing"
-  organization = tfe_workspace.name
+  organization = tfe_organization.tf-cloud-testing.name
   tag_names    = ["test", "app"]
 }
