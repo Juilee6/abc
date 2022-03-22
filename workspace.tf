a4a8b6fb59e07f8c0cb41e4593e71d760972015b
Juilee6 <81208483+Juilee6@users.noreply.github.com>
2022-03-22 15:48:37 +0530

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -1,28 +1,8 @@
-/*resource "tfe_organization" "tf-cloud-testing" {
-  name  = "tf-cloud-testing1"
-  email = "jkaloti@deqode.com"
-}
-
-resource "tfe_workspace" "test" {
-  name         = "jk-testing"
-  organization = tfe_organization.tf-cloud-testing.name
-  tag_names    = ["test", "app"]
-}*/
-terraform {
-  backend "remote" {
-    organization = tfe_organization.tf-cloud-testing
-
-    workspaces {
-      name = "my-workspace-name1"
-    }
-  }
-
-//  required_version = "~> 0.14"
-}
-
-resource "tfe_organization" "my_tf_cloud_org" {
-  name  = "my-tf-cloud-org"
-  email = "jkaloti@deqode.com"
-  tfe_token = zJKqTmpt9L4S8w.atlasv1.WOHd0ygS9Undzqk1OEolayaZhwyz2KvwC2Hu9wGhvjQN8ug7IfEphaCVcJkMKCWKXMQ
- // collaborator_auth_policy = "two_factor_mandatory"
+module "workspace" {
+  source  = "hashicorp-modules/workspace/tfe"
+  version = "0.1.0"
+  tfe_org_email = "jkaloti@deqode.com"
+  tfe_org_name = "tf-cloud-testing"
+  tfe_token = "zJKqTmpt9L4S8w.atlasv1.WOHd0ygS9Undzqk1OEolayaZhwyz2KvwC2Hu9wGhvjQN8ug7IfEphaCVcJkMKCWKXMQ"
+  
 }
