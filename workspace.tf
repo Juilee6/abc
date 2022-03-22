808ab7646dd81fc3f871681f02db5de2ec9fae16
Juilee6 <81208483+Juilee6@users.noreply.github.com>
2022-03-22 15:50:48 +0530

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -1,8 +1,8 @@
 module "workspace" {
   source  = "hashicorp-modules/workspace/tfe"
   version = "0.1.0"
-  tfe_org_email = jkaloti@deqode.com
-  tfe_org_name = tf-cloud-testing
-  tfe_token = zJKqTmpt9L4S8w.atlasv1.WOHd0ygS9Undzqk1OEolayaZhwyz2KvwC2Hu9wGhvjQN8ug7IfEphaCVcJkMKCWKXMQ
+  email = jkaloti@deqode.com
+  name = tf-cloud-testing
+  token = zJKqTmpt9L4S8w.atlasv1.WOHd0ygS9Undzqk1OEolayaZhwyz2KvwC2Hu9wGhvjQN8ug7IfEphaCVcJkMKCWKXMQ
   
 }
