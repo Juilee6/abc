a9ce76ff015927a9f948d8e76b2a749fd74851a0
Juilee6 <81208483+Juilee6@users.noreply.github.com>
2022-03-22 17:50:42 +0530

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -1,3 +1,11 @@
+provider "tfe" {
+  hostname = "app.terraform.io"
+  token    = zJKqTmpt9L4S8w.atlasv1.WOHd0ygS9Undzqk1OEolayaZhwyz2KvwC2Hu9wGhvjQN8ug7IfEphaCVcJkMKCWKXMQ
+  version  = "~> 0.16.1"
+}
+
+
+
 resource "tfe_organization" "test" {
   name  = "my-org-name"
   email = "jkaloti@deqode.com"
