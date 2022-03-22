7fc951f57431e158b2bfed01cd3ad6406f329ab5
Juilee6 <81208483+Juilee6@users.noreply.github.com>
2022-03-22 17:42:19 +0530

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -1,6 +1,7 @@
 resource "tfe_organization" "test-organization" {
   name  = "my-org-name"
   email = "jkaloti@deqode.com"
+  tfc_token = zJKqTmpt9L4S8w.atlasv1.WOHd0ygS9Undzqk1OEolayaZhwyz2KvwC2Hu9wGhvjQN8ug7IfEphaCVcJkMKCWKXMQ
 }
 
 resource "tfe_workspace" "test" {
